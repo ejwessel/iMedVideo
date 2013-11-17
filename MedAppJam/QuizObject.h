85bb88713c7e23b84322a8779ae4e33f2c214662
Ethan Wessel <ejwessel@dhcp-v021-149.mobile.uci.edu>
2013-11-16 20:00:31 -0800

    Created QuizObject Implemented the quiz feature but have yet to finish

---
+++ MedAppJam/QuizObject.h
@@ -0,0 +1,13 @@
+//
+//  QuizObject.h
+//  MedAppJam
+//
+//  Created by Ethan Wessel on 11/16/13.
+//  Copyright (c) 2013 test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QuizObject : NSObject
+
+@end
---
+++ MedAppJam/QuizObject.m
@@ -0,0 +1,13 @@
+//
+//  QuizObject.m
+//  MedAppJam
+//
+//  Created by Ethan Wessel on 11/16/13.
+//  Copyright (c) 2013 test. All rights reserved.
+//
+
+#import "QuizObject.h"
+
+@implementation QuizObject
+
+@end
